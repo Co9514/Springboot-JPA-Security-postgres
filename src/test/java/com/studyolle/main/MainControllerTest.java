338d9dab4cd4db815049253137ff52f19eba2ac5
Co9514 <khj950104@naver.com>
2020-05-21 19:34:32 +0900

    feat : 로그인 로그아웃 구현 Spring Security에서 제공하는 remember-me를 이용해 사용자의 로그인 기록을 유지하는 방법에 대해서 배웠다. 기존의 일반적인 쿠키만 사용하는 방식이 취약한 것은 알고있었고 다양한 대안들이 많이 나오기도했다. 이번에는 쿠키에 토큰과 유저이름, 그리고 시리즈까지 포함하여 해시암호화된 값을 쿠키로 사용하여 탈취당하여도 안전할 수 있도록 하는 방법이였는데 그렇다면 2곳에서 로그인이 시도되었을때에 어떻게 되는건지 테스트를 해보고싶다.

---
+++ src/test/java/com/studyolle/main/MainControllerTest.java
@@ -0,0 +1,91 @@
+package com.studyolle.main;
+
+import com.studyolle.account.AccountRepository;
+import com.studyolle.account.AccountService;
+import com.studyolle.account.SignUpForm;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MainControllerTest {
+    @Autowired
+    AccountRepository accountRepository ;
+    @Autowired
+    MockMvc mockMvc ;
+    @Autowired
+    AccountService accountService ;
+
+    @BeforeEach
+    void create_user(){
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setEmail("a93779501@gmail.com");
+        signUpForm.setNickname("Co9514");
+        signUpForm.setPassword("12345678");
+        accountService.processNewAccount(signUpForm);
+    }
+
+    @AfterEach
+    void delete_user(){
+        accountRepository.deleteAll();
+    }
+
+    @DisplayName("이메일 로그인 성공")
+    @Test
+    void email_login_success() throws Exception{
+        mockMvc.perform(post("/login")
+                .param("username","a93779501@gmail.com")
+                .param("password","12345678")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("Co9514"));
+    }
+    
+    @DisplayName("닉네임 로그인 성공")
+    @Test
+    void nickname_login_success() throws Exception{
+        mockMvc.perform(post("/login")
+                .param("username","Co9514")
+                .param("password","12345678")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("Co9514"));
+    }
+    
+    @DisplayName("로그인 실패")
+    @Test
+    void login_fail() throws Exception{
+        mockMvc.perform(post("/login")
+                .param("username","12312fad")
+                .param("password","1234567678")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"))
+                .andExpect(unauthenticated());
+    }
+
+    @DisplayName("로그아웃")
+    @Test
+    void logout() throws Exception{
+        mockMvc.perform(post("/logout")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(unauthenticated());
+    }
+}
